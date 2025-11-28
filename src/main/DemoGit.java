d84ce7e977a09f03fb629177a1644dbf3bfa09d1
NikitaAiwale <NikitaAiwale@Nikita-Aiwale>
2025-11-28 15:20:42 +0530

    second commit

--- src/main/DemoGit.java
+++ src/main/DemoGit.java
@@ -5,6 +5,10 @@ public class DemoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World 1");
+		
+		System.out.println("Hello World 2");
+
+		
 	}
 
 }
